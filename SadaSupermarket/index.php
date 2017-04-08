bebb749681cb732c66452f8ad8dcf8bf7fb38ff5
PoisonCMS <sabbamf@gmail.com>
2017-04-08 20:32:05 +0100

    Added Index.php

---
+++ SadaSupermarket/index.php
@@ -0,0 +1,85 @@
+<?php
+#####################################################
+##                                                 ##
+##                                                 ##
+##                                                 ##
+##            Copyrights © PosionCMS     		   ##
+##                                                 ##
+##                                                 ##
+##                                                 ##
+##                                                 ##
+#####################################################
+?>
+<?php
+include "include/header.php";
+?>
+
+
+
+<!-- Displays the header(image banner) -->
+<div class="header"></div>
+
+
+<div class="row" style="margin-top:1px;">
+
+<?php
+include "include/leftPanel.php";
+?>
+
+<!-- Where the Right col starts -->
+<div class="rightcol">
+
+		<div class="rightPanelTitle">Latest News</div><!-- Right Panel Title-->
+	<div class="rightPanelContent"><div style="text-align:center">
+	<!-- Where the Slider starts, -->
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+  <span class="dot active" onclick="currentSlide(1)"></span> 
+  <!-- Different Slides starts here -->
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span> 
+  <span class="dot" onclick="currentSlide(5)"></span> 
+  <a class="next" onclick="plusSlides(1)">❯</a>
+  
+  <!-- The content for the slides starts here -->
+<br><br></div><div class="mySlides fade" style="display: block;">
+<div class="topic_banner" style="background-image:url('/images/topic_banner.jpg')"> </div>
+
+
+  <div class="text"> <h1> 20% Offer on Fruits </h1> Ut vidit aperiam mea, vim ut dolor graece definitiones. Et alii dolorem usu. Ad est omnesque iracundia, sea reque iuvaret ut. Iriure reprimique eum in, ad has nulla possim, sed in alterum veritus. Te per nihil nusquam luptatum, ad nam appareat argumentum. <h1 style="text-align:right; font-size:12px;">2017-01-07 22:19:14 </h1> </div>
+</div><div class="mySlides fade" style="display: none;">
+<div class="topic_banner" style="background-image:url('/images/topic_banner.jpg')"> </div>
+
+
+  <div class="text"> <h1> 20% Offer on Fruits </h1> Ut vidit aperiam mea, vim ut dolor graece definitiones. Et alii dolorem usu. Ad est omnesque iracundia, sea reque iuvaret ut. Iriure reprimique eum in, ad has nulla possim, sed in alterum veritus. Te per nihil nusquam luptatum, ad nam appareat argumentum. <h1 style="text-align:right; font-size:12px;">2017-01-08 19:31:18 </h1> </div>
+</div><div class="mySlides fade" style="display: none;">
+<div class="topic_banner" style="background-image:url('/images/topic_banner.jpg')"> </div>
+
+
+  <div class="text"> <h1> Lorem Ipsum </h1> Ut vidit aperiam mea, vim ut dolor graece definitiones. Et alii dolorem usu. Ad est omnesque iracundia, sea reque iuvaret ut. Iriure reprimique eum in, ad has nulla possim, sed in alterum veritus. Te per nihil nusquam luptatum, ad nam appareat argumentum. <h1 style="text-align:right; font-size:12px;">2017-01-19 00:02:44 </h1> </div>
+</div><div class="mySlides fade" style="display: none;">
+<div class="topic_banner" style="background-image:url('/images/topic_banner.jpg')"> </div>
+
+
+  <div class="text"> <h1> Lorem Ipsum </h1> Ut vidit aperiam mea, vim ut dolor graece definitiones. Et alii dolorem usu. Ad est omnesque iracundia, sea reque iuvaret ut. Iriure reprimique eum in, ad has nulla possim, sed in alterum veritus. Te per nihil nusquam luptatum, ad nam appareat argumentum. <h1 style="text-align:right; font-size:12px;">2017-01-19 00:02:44 </h1> </div>
+</div><div class="mySlides fade" style="display: none;">
+<div class="topic_banner" style="background-image:url('/images/topic_banner.jpg')"> </div>
+
+
+  <div class="text"> <h1> Lorem Ipsum </h1> Ut vidit aperiam mea, vim ut dolor graece definitiones. Et alii dolorem usu. Ad est omnesque iracundia, sea reque iuvaret ut. Iriure reprimique eum in, ad has nulla possim, sed in alterum veritus. Te per nihil nusquam luptatum, ad nam appareat argumentum. <h1 style="text-align:right; font-size:12px;">2017-01-19 00:02:44 </h1> </div>
+</div></div></div></div>	
+
+
+
+<br>
+
+
+<br>
+
+
+
+
+
+<?php
+include "include/footer.php";
+?>
